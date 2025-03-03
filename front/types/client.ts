37473dbb659006902c8dcf56caa229d7ab96a9a3
Ngourndii-dev <hei.ainah.44@gmail.com>
2025-03-03 11:16:55 +0300

    suppression des autres directory que admine

--- front/types/client.ts
+++
@@ -1,5 +0,0 @@
-export interface Client {
-    client_name: string;
-    phone_number: string;
-    email: string;
-  }
--- front/types/review.ts
+++
@@ -1,8 +0,0 @@
-export interface Review {
-    id: number;
-    author: string;
-    id_animal: number;
-    rating: number;
-    comment: string;
-    status: "pending" | "available" | "unavailable";
-  }
